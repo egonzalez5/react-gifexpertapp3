9fa595e6b17bff66a03c3c37e6651b4c4a8062c3
Eduardo González <eduardo.gonzalez5599@gmail.com>
2020-10-21 00:15:03 -0300

    docs agregado

---
+++ docs/precache-manifest.16e3b7449809a29c82198a6eb836a21e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91161b6e4ecf317db2ab3ed131c3a59f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c85eda28e84682a40894",
+    "url": "/static/css/main.77b076ac.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c85eda28e84682a40894",
+    "url": "/static/js/main.cf587e8e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
